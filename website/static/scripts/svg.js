79e6af0d79b32450e5b0dd26e9cc1366fd90bfb8
Eben@ysh <77492217+EbenGitHub@users.noreply.github.com>
2022-12-03 12:23:02 +0300

    clean up portfolio svg

--- website/static/scripts/svg.js
+++ website/static/scripts/svg.js
@@ -8,15 +8,18 @@ $(document).ready(function(){
       let infoData = JSON.stringify(data)
       let nameset = Object.keys(JSON.parse(infoData))
       let dataset = Object.values(JSON.parse(infoData))
-
+      
+      // Set the width and height of the svg graph
       const w = 500;
       const h = 200;
-
+      
+      // instatiate the d3 javascript library
       const svg = d3.select("#cont-svg")
                     .append("svg")
                     .attr("width", w)
                     .attr("height", h)
-
+      
+      // Draw rectange svg for the bars graphs
       svg.selectAll("rect")
          .data(dataset)
          .enter()
@@ -29,7 +32,8 @@ $(document).ready(function(){
          .attr("class", "bar")
          .append("title")
          .text((d, i) => (`${d} ${nameset[i]}`))
-
+      
+      // Put the text above the graph for UI
       svg.selectAll("text")
          .data(dataset)
          .enter()
@@ -38,7 +42,8 @@ $(document).ready(function(){
          .attr("x", (d, i) => i * 90)
          .attr("y", (d, i) => h - (d * 3 + 9))
 
-
+         
+         // Put numbers in the html dynamically
          $("#user-number").text(dataset[nameset.indexOf("User")] - 1);
          $("#food-number").text(dataset[nameset.indexOf("Food")] - 1);
          $("#restaurant-number").text(dataset[nameset.indexOf("Restaurant")] - 1);
