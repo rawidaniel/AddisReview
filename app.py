b8f95a564e69ae4c52f836fd7d1375043d4221b2
Ebenezer Exetie <104176127+Eenrics@users.noreply.github.com>
2023-04-17 17:28:21 +0300

    let the host choose the port

--- app.py
+++ app.py
@@ -3,10 +3,13 @@
 """
 Run flask application
 """
+
+from os import getenv
 from website import create_app
 
+app_port = int(getenv("PORT", 5000))
 app = create_app()
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=app_port, debug=True)
